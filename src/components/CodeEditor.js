e50e05806e2592247bcfd205c3b581c42986fe65
francesco.dimarzo <francesco.dimarzo@lutech.it>
2024-02-16 17:42:32 +0100

    DRAFT: Add css linter

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -7,10 +7,10 @@ import { oneDark } from '@codemirror/theme-one-dark';
 import { css } from '@codemirror/lang-css';
 import { javascript } from '@codemirror/lang-javascript';
 import { html } from '@codemirror/lang-html';
-import { closeBrackets } from '@codemirror/autocomplete';
+import { closeBrackets, autocompletion } from '@codemirror/autocomplete';
 import { indentOnInput } from '@codemirror/language';
-import { searchKeymap, highlightSelectionMatches } from '@codemirror/search';
-import { lintKeymap } from '@codemirror/lint';
+import { lintKeymap, lintGutter, linter } from '@codemirror/lint';
+/* import { CSSLint } from '../utils/CSSLinter'; */
 
 const CodeEditor = ({ codeType, onChange }) => {
     const editor = useRef();
@@ -19,17 +19,30 @@ const CodeEditor = ({ codeType, onChange }) => {
     const onUpdate = EditorView.updateListener.of((v) => {
       setCode(v.state.doc.toString());
     });
-
+    
+    /* const onDiagnosticsChanged = EditorView.updateListener.of((update) => {
+      if (update.diagnosticsChanged) {
+        const errors = update.view.state.diagnostics.filter((d) => d.severity === 'error');
+        if (errors.length >  0) {
+          // Prevent saving if there are errors
+          console.error('Syntax errors found, snippet cannot be saved.');
+        } else {
+          // Save the snippet if there are no errors
+          console.log('No syntax errors, snippet can be saved.');
+        }
+      }
+    }); */
+    
     //TODO: Aggiungere altri linguaggi.
     const codeExtension = codeType === 'css' ? css() : codeType === 'javascript' ? javascript() : html();
-    
+    /* const cssLinter = CSSLint(); */
     const initialContent = 'Crea uno Snippet CSS'.repeat(1) + '\n'.repeat(9);
     
     useEffect(() => {
       const state = EditorState.create({
         doc: initialContent,
         extensions: [
-            keymap.of([defaultKeymap, indentWithTab, ...searchKeymap, ...lintKeymap]),
+            keymap.of([defaultKeymap, indentWithTab, ...lintKeymap]),
             lineNumbers(),
             highlightSpecialChars(),
             drawSelection(),
@@ -38,8 +51,11 @@ const CodeEditor = ({ codeType, onChange }) => {
             codeExtension,
             history(),
             closeBrackets(),
+            autocompletion(),
             indentOnInput(),
-            highlightSelectionMatches(),
+            /* linter(cssLinter), */
+            lintGutter(),
+            /* onDiagnosticsChanged, */
             onUpdate,
           ],
       });
