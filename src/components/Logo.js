cad586f90bb9c0676c8b6510223932e2a6f3f015
Barabba97 <fradm97@gmail.com>
2024-02-17 10:40:34 +0100

    Fix logo & navbar

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Logo = () => {
   return (
       <img
-        className="h-auto w-auto max-h-10 max-w-40"
-        src="/ICP_Logo.png"
+      className="h-auto w-auto max-h-7 max-w-30"
+      src="/ICP_Logo.png"
         alt="ICPLogo"
         id="logo"
       />
